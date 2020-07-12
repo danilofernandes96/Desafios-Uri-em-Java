1167f02db1efdc2b337852b56c48ccd1b5e35ab1
Danilo Fernandes <fernandes.danilo1996@gmail.com>
2020-07-12 12:03:20 -0400

    Desafio Uri Online 1003 a 1005

---
+++ 1003.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int a, b = 0;
+	   a = sc.nextInt();
+	   b = sc.nextInt();
+	   int soma = a + b;
+	    
+	   System.out.println("SOMA = "+soma);
+	}
+}
---
+++ 1005.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+	  double a = sc.nextDouble();
+	  double b = sc.nextDouble();
+	  double media = ((a * 3.5) + (b * 7.5)) / 11;
+	    
+	   System.out.printf("MEDIA = %.5f",media);
+	   System.out.println();
+	}
+}
